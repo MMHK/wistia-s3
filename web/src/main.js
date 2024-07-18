1d230a161e933cee37c5ee5730fcade62549c89e
MMHK <sam@mixmedia.com>
2024-07-18 15:31:14 +0800

    repack player bundle

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,6 @@
 (function (e) {
   window.MEDIA_ENDPOINT = e.MEDIA_ENDPOINT || undefined;
-  require("wistia-player-alternative/dist/js/main");
+  require("wistia-s3-player/dist/js/wistia-s3-player.min");
 })({
   ...window,
   MEDIA_ENDPOINT: "{{.MediaEndPoint}}"
